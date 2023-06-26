e28610a9502a1f9d7cfa69b41085259db78e665a
Igor De Nadai <igoroliveiradenadai@gmail.com>
2023-06-25 22:32:06 -0300

    plotmatrix
    now, in the end of simulation, the matrix is ploted. Check if the particle
    can move, and if it can not, generate other particle.

--- DLA_Final.py
+++ DLA_Final.py
@@ -1,7 +1,16 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
-matrix = np.zeros((10, 10))
+
+matrix = np.zeros((50, 50))
+
+def conferirPrimeiraLinha():
+    for i in matrix[0]:
+        if i == 1:
+            print("particula na primeira linha")
+            return True
 
 def simulacaoDLA():
     # Função para criar uma matriz 5x5 inicializada com zeros
@@ -26,11 +35,11 @@ def simulacaoDLA():
         if  matrix[LinhaAtual + 1][PosicaoAtual] == 1:
             matrix[LinhaAtual][PosicaoAtual] = 1
             print("colidiu")
-            return LinhaAtual, PosicaoAtual
+            return LinhaAtual, PosicaoAtual, True  # Colisão ocorreu, encerra a simulação
         else:
             matrix[LinhaAtual + 1][PosicaoAtual] = 1
             print("moveu pra baixo")
-            return PosicaoAtual, LinhaAtual + 1
+            return PosicaoAtual, LinhaAtual + 1, False
 
     # Função que move a partícula para a esquerda
     def moverParaEsquerda(LinhaAtual, PosicaoAtual):
@@ -38,15 +47,15 @@ def simulacaoDLA():
         if PosicaoAtual - 1 < 0:
             print("Atravessou pela esquerda")
             matrix[LinhaAtual][len(matrix[LinhaAtual]) - 1] = 1
-            return len(matrix[LinhaAtual]) - 1, LinhaAtual
+            return len(matrix[LinhaAtual]) - 1, LinhaAtual, False
         if matrix[LinhaAtual][PosicaoAtual - 1] == 1:
             matrix[LinhaAtual][PosicaoAtual] = 1
             print("colidiu")
-            return PosicaoAtual, LinhaAtual
+            return PosicaoAtual, LinhaAtual, True  # Colisão ocorreu, encerra a simulação
         else:
             matrix[LinhaAtual][PosicaoAtual - 1] = 1
             print("moveu pra esquerda")
-            return PosicaoAtual - 1, LinhaAtual
+            return PosicaoAtual - 1, LinhaAtual, False
 
     # Função que move a partícula para a direita
     def moverParaDireita(LinhaAtual, PosicaoAtual):
@@ -54,15 +63,15 @@ def simulacaoDLA():
         if PosicaoAtual + 1 == len(matrix[LinhaAtual]):
             matrix[LinhaAtual][0] = 1
             print("Atravessou pela direita")
-            return 0, LinhaAtual
+            return 0, LinhaAtual, False
         if matrix[LinhaAtual][PosicaoAtual + 1] == 1:
             matrix[LinhaAtual][PosicaoAtual] = 1
             print("colidiu")
-            return PosicaoAtual, LinhaAtual
+            return PosicaoAtual, LinhaAtual, True  # Colisão ocorreu, encerra a simulação
         else:
             matrix[LinhaAtual][PosicaoAtual + 1] = 1
             print("moveu pra direita")
-            return PosicaoAtual + 1, LinhaAtual
+            return PosicaoAtual + 1, LinhaAtual, False
 
     # Função que decide para onde a partícula vai se mover
     def decidirDirecao(direcaoDaParticula):
@@ -77,21 +86,32 @@ def simulacaoDLA():
         print(matrix)
         direcaoDaParticula = np.random.randint(0, 3)
         if direcaoDaParticula == 0:
-            PosicaoAtual, LinhaAtual = moverParaBaixo(LinhaAtual, PosicaoAtual)
+            PosicaoAtual, LinhaAtual, colisao = moverParaBaixo(LinhaAtual, PosicaoAtual)
         else:
-            PosicaoAtual, LinhaAtual = decidirDirecao(direcaoDaParticula)
+            PosicaoAtual, LinhaAtual, colisao = decidirDirecao(direcaoDaParticula)
+
+        if colisao:
+            print(matrix)
+            return True  # Colisão ocorreu, encerra a simulação
 
     print(matrix)
+    return False  # Simulação concluída sem colisão
 
-# Executar a simulação
-
-
-# while matrix[0][3] != 1:
-#     simulacaoDLA()
-
+#Executar a simulação
 i = 0
-while  i < 11:
-    simulacaoDLA()
-    print("fim da simulacao")
+while i < 11:
+    if simulacaoDLA():
+        print("Simulação encerrada devido à colisão.")
+        if conferirPrimeiraLinha():
+            break
+        else:
+            i = 0
+    else:
+        print("Simulação concluída sem colisão.")
     i += 1
- 
+
+cmapmine = ListedColormap(['w', 'b'], N=2)
+
+plt.matshow(matrix, cmap=cmapmine, vmin=0, vmax=1)
+
+plt.show()
